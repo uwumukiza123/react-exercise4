ef93560ebca25bec52ef3657f88cd28d30892952
uwumukiza123 <uwumukizalaetitia@gmail.com>
2023-10-01 04:48:37 +0200

    add react exercise4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
 function App() {
+  const [count, setCount] = React.useState(0);
+
+  function displayCount() {
+    setCount(count + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="button">
+      <h2>
+        Button has clicked: <span>{count} times </span>
+      </h2>
+      <button onClick={displayCount}>Click me</button>
     </div>
   );
 }
